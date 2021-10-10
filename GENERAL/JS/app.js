f626a5b15d74af3b82f01552dc5e73994d904835
ucemma <ucemma8@gmail.com>
2021-10-10 08:10:29 -0700

    first code

---
+++ GENERAL/JS/app.js
@@ -0,0 +1,10 @@
+let var1 = 3;
+let var2 = 4;
+var3 = var1 + var2;
+//console.log(var3);
+function myFunction(){
+    if (var1 == var2){
+        return "Equal variable";
+    }else {"not equal"};
+}
+console.log(myFunction);
